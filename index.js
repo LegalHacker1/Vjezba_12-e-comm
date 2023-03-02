486a8aa97b274206b55abed08b5ea29ce6f894a4
Goran Bjelica <goranbjelica303@gmail.com>
2023-03-02 14:48:03 +0100

    Created a localhost server, added GET and POST methods, HTML form

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+    <div>
+        <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name"passwordComfirmation" placeholder="password comfirmation" />
+        <button>Sign up</button>
+        </form>
+    </div>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Account created!!!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening');
+});
